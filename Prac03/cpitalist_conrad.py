690269f016ef0574e6d8169cf585463b0d9dd138
AlexCSIlva <alexander.silva@my.jcu.edu.au>
2016-03-18 14:18:41 +1000

    fixed few errors

--- Prac03/cpitalist_conrad.py
+++ Prac03/cpitalist_conrad.py
@@ -2,7 +2,7 @@ import random
 
 MAX_INCREASE = 0.1 # 10%
 MAX_DECREASE = 0.05 # 5%
-MIN_PRICE = 0.001
+MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
@@ -22,5 +22,5 @@ while price >= MIN_PRICE and price <= MAX_PRICE:
         # between negative MAX_INCREASE and 0
         priceChange = random.uniform(-MAX_DECREASE, 0)
 
-print *= (1, priceChange)
-print("${:,.2f}".format(price))
+    price *= (1 + priceChange)
+    print("${:,.2f}".format(price))
