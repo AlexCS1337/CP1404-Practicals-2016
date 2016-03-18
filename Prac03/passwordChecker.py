723244cae3b73cd202b4e8051ea2da3127e674ee
AlexCSIlva <alexander.silva@my.jcu.edu.au>
2016-03-18 15:13:02 +1000

    initial commit

---
+++ Prac03/passwordChecker.py
@@ -0,0 +1,23 @@
+__author__ = 'Alex'
+
+""" Password Checker written by Alex
+
+"""
+
+MINIMUM_LENGTH = 6
+MAXIMUM_LENGTH = 15
+
+
+print("Please enter a valid password")
+print("Your password must be between 6 and 15 characters, and contain:\n"
+      "1 or more uppercase characters\n"
+      "1 or more lowercase characters\n"
+      "1 or more number/s\n"
+      "and 1 or more special characters: !@#$%^^&*()_[]")
+
+password = input(">")
+while len(password) < MINIMUM_LENGTH or len(password) > MAXIMUM_LENGTH:
+    print("Invalid password!")
+    password = input(">")
+
+print("Your 10 character password is valid:", password)
