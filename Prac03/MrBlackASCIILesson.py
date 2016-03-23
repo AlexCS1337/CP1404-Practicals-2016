8406a31a5323868cc6bb3da28c2fb4f5bb5b2891
jc282222 <alexander.silva@my.jcu.edu.au>
2016-03-23 13:41:24 +1000

    added get_number function

--- Prac03/MrBlackASCIILesson.py
+++ Prac03/MrBlackASCIILesson.py
@@ -1,3 +1,10 @@
 lower = 10
 upper = 100
 print("Enter a number ({0}-{1}):".format(lower, upper))
+
+def get_number(lower, upper):
+    number = (input("Enter a number (10-50): "))
+    while number.isdecimal() != True:
+        print("Enter a valid number!")
+        number = (input("Enter a number (10-50): "))
+get_number(10, 50)
