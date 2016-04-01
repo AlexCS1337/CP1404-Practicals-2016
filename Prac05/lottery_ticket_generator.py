1f535febf3ec7ee09bba3cbf0b0bb1a3c3b96ee7
jc282222 <alexander.silva@my.jcu.edu.au>
2016-04-01 14:31:22 +1000

    changed a variable

--- Prac05/lottery_ticket_generator.py
+++ Prac05/lottery_ticket_generator.py
@@ -4,12 +4,12 @@ NUMBERS_PER_LINE = 6
 MINIMUM = 1
 MAXIMUM = 45
 
-quickPicks = int(input("How many quick picks? "))
-while quickPicks < 0:
+choice = int(input("How many quick picks? "))
+while choice < 0:
     print("That makes no sense!")
     quickPicks = int(input("How many quick picks? "))
 
-for i in range(quickPicks):
+for i in range(choice):
     for i in range(NUMBERS_PER_LINE):
         number = random.randint(MINIMUM, MAXIMUM)
         print(format(number, "2d"), end ="")
