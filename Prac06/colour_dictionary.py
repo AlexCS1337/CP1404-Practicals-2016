7fdbf03621dfbc29250b303d9ab7a73bfa3b3617
jc282222 <alexander.silva@my.jcu.edu.au>
2016-04-08 12:57:23 +1000

    Initial commit

---
+++ Prac06/colour_dictionary.py
@@ -0,0 +1,11 @@
+COLOUR_CODES = {"Purple": "660066", "Cream Green": "9aff9a", "Cyan": "a0ffee", "Grey": "666666",
+                "Green": "46a346", "Aqua": "008272", "Blue": "36aebc", "Pink": "f45cff"}
+#print(COLOUR_CODES)
+
+colour = input("Enter colour: ").upper()
+while colour != "":
+    if colour in COLOUR_CODES:
+        print(colour, "is", COLOUR_CODES[colour])
+    else:
+        print("Colour not found.")
+    colour = input("Enter colour: ").upper()
