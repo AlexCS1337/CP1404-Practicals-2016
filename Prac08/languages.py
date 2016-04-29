b247d8bf97b2ea2677acee23308962d9e62ecc8e
jc282222 <alexander.silva@my.jcu.edu.au>
2016-04-29 14:42:04 +1000

    tested __str__ function and print list of dynamic languages

--- Prac08/languages.py
+++ Prac08/languages.py
@@ -5,3 +5,10 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
 languages = [ruby, python, vb]
+
+print(python)
+
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
