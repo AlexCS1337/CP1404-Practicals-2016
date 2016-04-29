a6161b3ffab8adde542c9618ecee87786b109a6e
jc282222 <alexander.silva@my.jcu.edu.au>
2016-04-29 15:47:11 +1000

    fixed for loop

--- Prac08/guitar_test.py
+++ Prac08/guitar_test.py
@@ -10,4 +10,4 @@ guitars.append(Guitar("Gibson L-5 CES", 1992, 16035.40))
 guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
 for i, guitar in enumerate(guitars):
-    print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}".format(i + 1, guitar.name, guitar.year, guitar.cost))
+    print("Guitar {}: {:>20} ({}), worth ${:10,.2f}".format(i + 1, guitar.name, guitar.year, guitar.cost))
