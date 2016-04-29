3072365092512d700559e6573f408346524a7444
jc282222 <alexander.silva@my.jcu.edu.au>
2016-04-29 15:42:07 +1000

    Initial test of Guitar class

--- Prac08/guitar.py
+++ Prac08/guitar.py
@@ -1,11 +1,11 @@
 class Guitar:
-    def __int__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-        return "{} ({}) : {}".format(self.name, self.year, self.cost)
+        return "{} ({}) : ${}".format(self.name, self.year, self.cost)
 
     def get_age(self):
         return 2016 - self.year
