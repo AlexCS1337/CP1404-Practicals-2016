82116123ee09066dd681866b53133e127ab5344e
AlexCSilva <alexander.silva@my.jcu.edu.au>
2016-06-06 00:37:29 +1000

    Initial commit

---
+++ PractiseProjects/sampleExampleQuestions2.py
@@ -0,0 +1,23 @@
+""" Write Python code for the following:
+Ask the user for their age, then print the square of each number from the age down to zero
+
+"""
+
+""" Write a function that takes two integer input parameters, adds up the numbers between these (inclusive) and returns the total. If the first number is not less than the second number it should return 0.
+Example if the parameters are (9, 11), it should return 30
+"""
+
+
+""" Write a function to count the number of words in a string (ignoring punctuation), e.g.
+	s = "This is a sentence with words in it"
+	print(countWords(s)) #displays 8
+"""
+
+
+""" Write a function that takes in one integer parameter, n, and returns a list of n unique random numbers between 1 and 10 * n
+"""
+
+
+""" Write pseudocode for a program that asks the user for a string then displays every letter in the string twice. E.g. if they input "hello exam" it should display "hheelllloo  eexxaamm"
+"""
+
